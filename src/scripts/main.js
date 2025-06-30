050e34c16da886074e2a97f58694aaa7efdac07d
Aweiro <arsenskott@gmail.com>
2025-06-30 23:26:16 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelectorAll('.tree li');
+
+for (const key of tree) {
+  const span = document.createElement('span');
+
+  span.textContent = key.firstChild.textContent;
+  key.firstChild.textContent = '';
+  key.insertAdjacentElement('afterbegin', span);
+
+  let isOn = false;
+
+  span.addEventListener('click', () => {
+    isOn = !isOn;
+
+    const keys = key.querySelectorAll('ul');
+
+    if (isOn) {
+      keys.forEach((element) => (element.style.display = 'none'));
+    } else {
+      keys.forEach((element) => (element.style.display = 'block'));
+    }
+  });
+}
