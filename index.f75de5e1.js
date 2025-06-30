966a3ab56ea85eb05f017566eaf418f7471f60bf
Aweiro <arsenskott@gmail.com>
2025-06-30 23:42:32 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const tree = document.querySelectorAll(".tree li");
+for (const key of tree){
+    const span = document.createElement("span");
+    span.textContent = key.firstChild.textContent;
+    key.firstChild.textContent = "";
+    key.insertAdjacentElement("afterbegin", span);
+    span.addEventListener("click", ()=>{
+        const keys = key.querySelectorAll("ul");
+        keys.forEach((element)=>element.hidden = !element.hidden);
+    });
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
